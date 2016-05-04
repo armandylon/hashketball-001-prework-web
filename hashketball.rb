b7899e7ea275bedfecd22162254f7ca69c012a17
armandylon <dire26463@gmail.com>
2016-05-04 19:07:38 +0000

    nitrous-backup at Wed May  4 19:07:38 UTC 2016

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,23 @@
-# Write your code here!
+def game_hash
+  game_hash =
+    {
+      :top_level_keys => {
+        :home
+        :away
+       },
+
+        :team_level_keys => {
+          :team_name
+          :colors
+          :players
+
+        }
 
 
+
+      }
+
+      end
 
 
 
